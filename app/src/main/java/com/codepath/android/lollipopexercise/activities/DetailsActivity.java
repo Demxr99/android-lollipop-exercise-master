1e86dd7ed3666b55bf148af236f67ea2a6d947a0
Demar Edwards <dedwards@fb.com>
2018-07-12 11:15:08 -0700

    Added palette

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,13 +1,17 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.codepath.android.lollipopexercise.R;
 import com.codepath.android.lollipopexercise.models.Contact;
 
@@ -36,6 +40,34 @@ public class DetailsActivity extends AppCompatActivity {
         Glide.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).centerCrop().into(ivProfile);
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+
+
+        // Define a listener for image loading
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                // TODO 1. Insert the bitmap into the profile image view
+                ivProfile.setImageBitmap(resource);
+
+                // TODO 2. Use generate() method from the Palette API to get the vibrant color from the bitmap
+                // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                Palette palette = Palette.from(resource).generate();
+                // Pick one of the swatches
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                if (vibrant != null) {
+                    // Set the background color of a layout based on the vibrant color
+                    vPalette.setBackgroundColor(vibrant.getRgb());
+                    // Update the title TextView with the proper text color
+                    tvName.setTextColor(vibrant.getTitleTextColor());
+                }
+
+            }
+        };
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+        ivProfile.setTag(target);
+        // Instruct Picasso to load the bitmap into the target defined above
+        Glide.with(this).load(mContact.getThumbnailDrawable()).asBitmap().centerCrop().into(target);
+
     }
 
     @Override
--- app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -2,6 +2,9 @@ package com.codepath.android.lollipopexercise.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +13,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
 import com.codepath.android.lollipopexercise.models.Contact;
 
 import java.util.List;
+
+import static com.codepath.android.lollipopexercise.activities.DetailsActivity.EXTRA_CONTACT;
 
 // Provide the underlying view for an individual list item.
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
@@ -37,11 +45,43 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
 
     // Display data at the specified position
     @Override
-    public void onBindViewHolder(VH holder, int position) {
+    public void onBindViewHolder(final VH holder, int position) {
         Contact contact = mContacts.get(position);
         holder.rootView.setTag(contact);
         holder.tvName.setText(contact.getName());
-        Glide.with(mContext).load(contact.getThumbnailDrawable()).centerCrop().into(holder.ivProfile);
+//        Glide.with(mContext).load(contact.getThumbnailDrawable()).centerCrop().into(holder.ivProfile);
+
+        // Use Glide to get a callback with a Bitmap which can then
+        // be used to extract a vibrant color from the Palette.
+
+        // Define a listener for image loading
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                // TODO 1. Insert the bitmap into the profile image view
+                holder.ivProfile.setImageBitmap(resource);
+
+                // TODO 2. Use generate() method from the Palette API to get the vibrant color from the bitmap
+                // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                Palette palette = Palette.from(resource).generate();
+                // Pick one of the swatches
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                if (vibrant != null) {
+                    // Set the background color of a layout based on the vibrant color
+                    holder.vPalette.setBackgroundColor(vibrant.getRgb());
+                    // Update the title TextView with the proper text color
+                    holder.tvName.setTextColor(vibrant.getTitleTextColor());
+                }
+            }
+        };
+
+        // TODO: Clear the bitmap and the background color in adapter
+
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+        holder.ivProfile.setTag(target);
+        // Instruct Picasso to load the bitmap into the target defined above
+        Glide.with(mContext).load(contact.getThumbnailDrawable()).asBitmap().centerCrop().into(target);
     }
 
     @Override
@@ -70,6 +110,9 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
                     final Contact contact = (Contact)v.getTag();
                     if (contact != null) {
                         // Fire an intent when a contact is selected
+                        Intent intent = new Intent(context, DetailsActivity.class);
+                        intent.putExtra(EXTRA_CONTACT, contact);
+                        context.startActivity(intent);
                         // Pass contact object in the bundle and populate details activity.
                     }
                 }
